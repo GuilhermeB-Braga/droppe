040b98cc8663b8810d4db0da63746eb796e395f3
Guilherme Braga <bbraga.guilherme@gmail.com>
2026-01-31 23:25:10 -0300

    Improve responsiveness on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,23 @@ import SVGComponent from "@/components/SVGComponent";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background flex gap-15 p-15">
-      <div className="flex-1">
+    <main
+      className="
+      min-h-screen bg-background flex flex-col items-center justify-center
+      md:flex-row md:gap-15 px-7.5 md:px-15 py-5"
+    >
+      <div className="flex-1 w-full">
         <SVGComponent />
       </div>
 
-      <div className="w-[50%] flex flex-col gap-7.5 p-7.5">
-        <h1 className="text-[32px] font-medium">
-          Crie uma seção e <span className="text-primary italic">droppe</span> seus arquivos :)
+      <div className="md:w-[50%] flex flex-col gap-7.5">
+        <h1 className="text-[28px] md:text-[32px] font-medium">
+          Crie uma seção e <span className="text-primary italic">droppe</span>{" "}
+          seus arquivos :)
         </h1>
 
         {/* Formulário - ADAPTAR DEPOIS */}
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-5 md:gap-2">
           <CreateSectionForm />
 
           <div className="flex gap-5 items-center text-text-muted">
